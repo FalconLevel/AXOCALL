9508899d23daf9edcf95e50070c09284703b864c
accounts15 <accounts15@kodego.ph>
2025-06-20 22:45:25 +0800

    Removed Secrets

--- config/twilio.php
+++ config/twilio.php
@@ -3,10 +3,10 @@
 
     return [
         'twilio' => [
-            'url' => env('TWILIO_URL', 'https://api.twilio.com'),
-            'sid' => env('TWILIO_SID', 'ACcb48f3a256fc761e85c317e25bb831a2'),
-            'token' => env('TWILIO_TOKEN', '665d85e7f68ac630d48ff195252945fe'),
-            'number' => env('TWILIO_NUMBER', '2392968674|8557505972'),
+            'url' => env('TWILIO_URL'),
+            'sid' => env('TWILIO_SID'),
+            'token' => env('TWILIO_TOKEN'),
+            'number' => env('TWILIO_NUMBER'),
         ],
         'svg' => [
             'inbound' => '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-5 w-5 text-blue-500" data-lov-id="src\components\call-logs\CallLogTable.jsx:369:14" data-lov-name="PhoneIncoming" data-component-path="src\components\call-logs\CallLogTable.jsx" data-component-line="369" data-component-file="CallLogTable.jsx" data-component-name="PhoneIncoming" data-component-content="%7B%22className%22%3A%22h-5%20w-5%20text-blue-500%22%7D" title="Inbound"><polyline points="16 2 16 8 22 8"></polyline><line x1="22" x2="16" y1="2" y2="8"></line><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>',
