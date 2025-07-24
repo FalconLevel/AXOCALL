a09f0d5826e364c0fb2a54ee527cc151b3bbd310
accounts15 <accounts15@kodego.ph>
2025-07-24 23:08:10 +0800

    Fix Roles Issue

--- app/Helpers/GlobalHelper.php
+++ app/Helpers/GlobalHelper.php
@@ -586,7 +586,7 @@ class GlobalHelper {
 
     public function getPermissions() {
         try {
-            $user = User::find(Auth::id())->first();
+            $user = User::find(auth()->user()->id)->first();
             return json_decode(Role::where('role', $user->role)->first()->toArray()['permissions'], true);
         } catch (\Exception $e) {
             logInfo($e->getMessage());
